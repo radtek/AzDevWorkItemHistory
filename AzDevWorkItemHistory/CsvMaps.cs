3b589f4e32fbb76aa3484e68060cc3e6371c9da1
Jon Fuller <jcfuller@sep.com>
2019-08-21 15:44:11 -0400

    reorder output CSV for revisions and durations - makes it easier to paste into my actual destination

--- AzDevWorkItemHistory/CsvMaps.cs
+++ AzDevWorkItemHistory/CsvMaps.cs
@@ -1,22 +1,26 @@
 ﻿using System;
+using System.Globalization;
 using CsvHelper.Configuration;
 using LanguageExt;
 
 namespace WorkItemHistory
 {
-    public class CsvMaps
+    public static class CsvMaps
     {
-        public class WorkItemInfoMap : ClassMap<WorkItemInfo>
+        public sealed class WorkItemInfoMap : ClassMap<WorkItemInfo>
         {
             public WorkItemInfoMap()
             {
                 AutoMap(new Configuration {MemberTypes = MemberTypes.Fields});
-                Map(m => m.Start).ConvertUsing(d => DateTimeString(d.Start));
-                Map(m => m.End).ConvertUsing(d => DateTimeString(d.Start));
+                Map(m => m.Start).Index(1).ConvertUsing(d => DateTimeString(d.Start));
+                Map(m => m.End).Index(0).ConvertUsing(d => DateTimeString(d.Start));
+                Map(m => m.WorkItemType).Index(2);
+                Map(m => m.Id).Index(3);
+                Map(m => m.Title).Index(4);
             }
             string DateTimeString(Option<DateTime> item)
             {
-                return item.Map(d => d.ToString()).IfNone(string.Empty);
+                return item.Map(d => d.ToString(CultureInfo.CurrentCulture)).IfNone(string.Empty);
             }
         }
     }
