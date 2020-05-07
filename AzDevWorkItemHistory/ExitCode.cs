9ce84b89ef5e8ef2aa6d48b74d0f593964136458
Jon Fuller <jcfuller@sep.com>
2020-05-07 11:23:37 -0400

    enhance logging when unauthorized

--- AzDevWorkItemHistory/ExitCode.cs
+++ AzDevWorkItemHistory/ExitCode.cs
@@ -10,7 +10,7 @@ namespace WorkItemHistory
         public static ExitCode GeneralException(Exception e) => new ExitCode(-1, e.ToString());
         public static ExitCode DuplicateUri(Uri uri) => new ExitCode(2, $"This URI ({uri}) already exists. You need to 'logout' with that URI if you wish to update the credentials for that URI.");
         public static ExitCode NeedToLogin(Uri uri) => new ExitCode(3, $"You'll need to login to {uri} first to perform this operation.");
-        public static ExitCode Unauthorized(VssUnauthorizedException exception, Uri azureUri) => new ExitCode(4, $"You need to re-login to {azureUri}. Your access token as either been revoked or expired.");
+        public static ExitCode Unauthorized(VssUnauthorizedException exception, Uri azureUri) => new ExitCode(4, $"You need to re-login to {azureUri}. Your access token as either been revoked or expired. Full exception below.{Environment.NewLine}{Environment.NewLine}{exception}");
 
         private ExitCode(int value, string message)
         {
